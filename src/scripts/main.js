86a8c9fbc48eaa6dd950f158a0edd28c3ced2626
Jailson Roth <104799776+Jailsonr12@users.noreply.github.com>
2026-01-13 17:39:00 -0300

    Add event sections and countdown timer, refactor styles
    Introduces multiple event sections to index.html with AOS animations and a dynamic countdown timer in main.js. Refactors SCSS structure: moves and renames event and hero styles, adds variable.scss for color management, and updates components to use variables. Adds a build script to package.json.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-console.log("Hello, World!");
+AOS.init();
+const dataDoEvento = new Date("2026-08-17T19:00:00")
+const timeStampDoEvento = dataDoEvento.getTime()
+
+const cronometro = setInterval(function(){
+    const agora = new Date()
+    const timeStampAtual = agora.getTime();
+
+    if (timeStampAtual > timeStampDoEvento){
+        const contador = "Evento ja começou"
+        document.getElementById("dataDoEvento").innerHTML = contador
+        clearInterval(cronometro)
+        return
+    }
+
+    const diaEmMs= 1000 *60 *60 *24;
+    const horasEmMs= 1000 *60 *60;
+    const minutosEmMs= 1000 * 60;
+    const segundosEmMs= 1000
+    
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs);
+    const contador = diasAteOEvento + " Dias " + horasAteOEvento + " Horas " + minutosAteOEvento + " Minutos " + segundosAteOEvento + " Segundos."
+
+    document.getElementById("dataDoEvento").innerHTML = contador
+
+
+}, 1000);
